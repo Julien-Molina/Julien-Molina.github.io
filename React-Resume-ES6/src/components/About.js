8416337456cf529a2a7ebb28f590dbd8dbca3db0
Julien Molina <julien@molina.pro>
2017-07-10 09:43:03 +0200

    Fix & Publish

--- React-Resume-ES6/src/components/About.js
+++ React-Resume-ES6/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = props => {
     return (
       <section className="about">
-        <h2 className="text-uppercase"><i className="fa fa-lg fa-user"></i> About</h2>
+        <h2 className="text-uppercase"><i className="fa fa-lg fa-user"></i> Présentation</h2>
         <div>{props.aboutData}</div>
       </section>
     );
